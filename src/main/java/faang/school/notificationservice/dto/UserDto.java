5b1b5d485a85eecc05af154f5be97c45589c6fed
Matvey <mashptz@gmail.com>
2024-12-08 16:48:27 +0300

    draft

--- src/main/java/faang/school/notificationservice/dto/UserDto.java
+++ src/main/java/faang/school/notificationservice/dto/UserDto.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDto {
+public class    UserDto {
     private long id;
     private String username;
     private String email;
